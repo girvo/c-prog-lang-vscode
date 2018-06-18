cb178158efda4901cce5a3912638ce3f0a05fb59
Josh Girvin <josh@jgirvin.com>
2018-06-18 20:15:30 +1000

    Finished page 9

--- temp.c
+++ temp.c
@@ -2,12 +2,17 @@
 
 int main()
 {
-  int fahr, celcius;
-  int lower, upper, step;
+    int fahr, celcius;
+    int lower, upper, step;
 
-  lower = 0;    /* lower limit of temp table */
-  upper = 300;  /* upper limit */
-  step = 20;
+    lower = 0;    /* lower limit of temp table */
+    upper = 300;  /* upper limit */
+    step = 20;
 
-  fahr = lower;
+    fahr = lower;
+    while (fahr <= upper) {
+        celcius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celcius);
+        fahr = fahr + step;
+    }
 }
